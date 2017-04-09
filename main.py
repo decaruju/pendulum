4ada23187f910490ac29b586cfbfdb05902ef261
Julien de Carufel <julien.decarufel@gmail.com>
2017-04-09 14:57:16 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import tkinter as tk
+
+from pendulum import Pendulum
+from point import Point
+
+size = 1960/2, 1080/2
+master = tk.Tk()
+canvas = tk.Canvas(master, width=size[0], height=size[1])
+master.attributes('-fullscreen', True)
+canvas.pack()
+pend1 = Pendulum(Point(size[0] / 2, size[1] / 2), length=200)
+pend2 = Pendulum(Point(0, 0), length=250)
+points = []
+
+
+def draw_point():
+    global canvas, pend1, points
+    canvas.delete('all')
+    for point in points:
+        width = point[0] / 10
+        x, y = point[1].x, point[1].y
+        canvas.create_oval(x - width, y - width, x + width, y + width, fill='#000000')
+        point[0] -= 1
+    try:
+        point = points[-1]
+        x, y = point[1].x, point[1].y
+        canvas.create_line(x, y, pend1.get_end().x, pend1.get_end().y)
+    except:
+        pass
+    canvas.create_line(size[0] / 2, size[1] / 2, pend1.get_end().x, pend1.get_end().y)
+    points = [p for p in points if p[0] > 10]
+
+def update():
+    global canvas, master, pend1
+    draw_point()
+    pend1.update(pend=pend2)
+    pend2.update(center=pend1.get_end())
+    points.append([1, pend1.get_end()])
+    points.append([100, pend2.get_end()])
+    master.after(20, update)
+
+
+def main():
+    update()
+    tk.mainloop()
+
+
+if __name__ == '__main__':
+    main()
---
+++ pendulum.py
@@ -0,0 +1,27 @@
+from math import cos, sin, pi
+
+from point import Point
+
+class Pendulum:
+    def __init__(self, center, length, angle=0, speed=0):
+        self.center = center
+        self.angle = angle
+        self.speed = speed
+        self.length = length
+
+    def get_end(self):
+        return Point(self.center.x + self.length * cos(self.angle), self.center.y + self.length * sin(self.angle))
+
+    def update(self, center=None, pend=None):
+        speed = 0.001
+        if center is not None:
+            self.center = center
+        if pend is not None:
+            self.speed += speed*cos(self.angle - pend.angle)
+        while self.angle < 0:
+            self.angle += 2*pi
+        while self.angle > 2*pi:
+            self.angle -= 2*pi
+        self.angle += self.speed
+        self.speed += speed*cos(self.angle)
+
